99c84b0f26c7328426b618588809e2b0527b25a7
Kevin Aubel <kevin.aubel@nova-technology.fr>
2023-05-22 10:24:39 +0200

    feat: test meilleur params

--- web/main.py
+++ web/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # set upload folder
 UPLOAD_FOLDER = 'static/images'
 CHECKPOINT_DIR = 'saved_model/'
-MODEL_SAVED = CHECKPOINT_DIR + 'zoidberg_model.h5'
+MODEL_SAVED = CHECKPOINT_DIR + 'latest_model.h5'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # # set variable for model
@@ -43,7 +43,7 @@ def index():
 
 def get_prediction(image_name):
     img = tf.keras.utils.load_img(
-        image_name, target_size=(224, 224)
+        image_name, target_size=(128, 128), color_mode="grayscale"
     )
     img_array = tf.keras.utils.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
